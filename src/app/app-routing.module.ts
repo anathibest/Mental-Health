9a9f0b875380e068ffe87fbfc0aa2765b528bb3e
Asisipho Daraza <daraza@thedigitalacademy.co.za>
2021-10-01 14:40:31 +0200

    thoughtsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 // Added Components
-import { HomeComponent } from './components/home/home.component';
-
+import { HomeComponent } from "./components/home/home.component";
+import { ThoughtRecordComponent } from "./components/thoughtRecord/thoughtRecord.component";
 
 const routes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: '', pathMatch: 'full', redirectTo: '/' },
-	{ path: '', pathMatch: 'full', redirectTo: '/' },
-	
+  { path: "", component: HomeComponent },
+  { path: "", pathMatch: "full", redirectTo: "/" },
+  { path: "", pathMatch: "full", redirectTo: "/" },
+  { path: "thoughs", component: ThoughtRecordComponent },
 
-	// { path: '404', component:},
-	{ path: '**', redirectTo: '/404' }
+  // { path: '404', component:},
+  { path: "**", redirectTo: "/404" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-
 export class AppRoutingModule {}
