a500eea28eb33a676ada561c1e90b8f530c9f755
anathi best <best@thedigitalacademy.co.za>
2021-10-06 22:16:45 +0200

    quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { ThoughtRecordComponent } from "./components/thoughtRecord/thoughtRecord.component";
 import { LiveConsultantComponent } from "./live-consultant/live-consultant.component";
+import { QuizzComponent } from "./components/quizz/quizz.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "", pathMatch: "full", redirectTo: "/" },
   { path: "", pathMatch: "full", redirectTo: "/" },
-  { path: "thoughs", component: ThoughtRecordComponent },
-  { path: "charts", component: LiveConsultantComponent },
+  { path: "thoughts", component: ThoughtRecordComponent },
+  { path: "chats", component: LiveConsultantComponent },
+  { path: "quizz", component: QuizzComponent },
 
   // { path: '404', component:},
   { path: "**", redirectTo: "/404" },
